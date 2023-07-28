b04b23a9a2869c1f076f6b41eb154735bb85823c
Hilmianugrah <hilmianugrah2003@gmail.com>
2023-07-28 10:53:55 +0700

    delete ponpes rar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,11 @@ export default {
   content: ["./*.{php,html}", "./src/**/*.{html,php}"],
   theme: {
     extend: {},
+    colors: {
+      'body' : '#F0F2F9',
+      'main' : '#00BCF1'
+    },
   },
   plugins: [],
 };
 
-
-module.exports = {
-  theme: {
-    colors: {
-      body : '#F0F2F9'
-    }
-  }
-}
