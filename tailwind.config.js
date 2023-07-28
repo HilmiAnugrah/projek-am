a8a8332326c2486f371118c079a0849287e3ab9f
Hilmianugrah <hilmianugrah2003@gmail.com>
2023-07-28 09:43:22 +0700

    hayde

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{php,html}", "./src/**/*.{html,php}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
+module.exports = {
+  theme: {
+    colors: {
+      body : '#F0F2F9'
+    }
+  }
+}
