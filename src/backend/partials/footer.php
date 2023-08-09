df2aa446934c895ab747ed76a42bf1b8e3470344
Hilmianugrah <hilmianugrah2003@gmail.com>
2023-08-09 17:01:16 +0700

    menyelesaikan footer

--- src/backend/partials/footer.php
+++ src/backend/partials/footer.php
@@ -43,8 +43,8 @@
 
     </div>
     <!-- end menu footer -->
-    <div class="mx-auto w-full pt-[80px] sm:pt-[120px] pb-10 md:flex md:flex-row justify-between ">
-        <div class="w-full ">
+    <div class="mx-auto w-full sm:w-[90%] md:w-[90%] lg:w-[80%] pt-[80px] sm:pt-[120px] pb-10 flex flex-col md:flex-row justify-around lg:gap-5 md:gap-2">
+        <div class=" w-full lg:w-[60%] md:w-[70%]">
             <div class="flex flex-row items-center justify-center w-full md:justify-start md:ml-[10%]">
                 <img src="src/img/logo.svg" alt="Logo Al Ashr Al Madani" class="w-16">
                 <h2 class="ml-2 text-xl font-semibold ">Al 'Ashr Al Madani</h2>
@@ -53,35 +53,35 @@
                 <p class="text-sm text-center px-[10%] tracking-wide md:text-left">Mencetak kader Hafizh hafizhoh,Qori qori’ah, Da’i da’iyyah yang memiliki keterampilan berbahasa Arab, Inggris dan Perancis yang berakhlak mulia.</p>
             </div>
 
-            <div class="contact w-full flex flex-row gap-4 justify-center mt-3 md:justify-start md:ml-[10%] ">
+            <div class="contact w-full flex flex-row gap-4 justify-center mt-3 md:justify-start md:ml-[10%]">
                 <div class="whatsapp flex flex-row gap-2 items-center justify-center">
-                    <img src="src/img/icons/whatsapp.svg" alt="wahstapp" class="w-8 h-8">
+                    <img src="src/img/icons/whatsapp.svg" alt="wahstapp" class="w-8 h-8 md:w-5 md:h-5">
                     <h3 class="text-sm font-semibold">0895601616644</h3>
                 </div>
                 <div class="phone flex flex-row gap-2 items-center justify-center ">
-                    <img src="src/img/icons/phone.svg" alt="Phone" class="w-8 h-8">
+                    <img src="src/img/icons/phone.svg" alt="Phone" class="w-8 h-8 md:w-5 md:h-5">
                     <h3 class="text-sm font-semibold">0895601616644</h3>
                 </div>
             </div>
             <div class="sosial-media w-full flex flex-row justify-center items-center gap-x-5 mt-4 md:justify-start md:ml-[10%] ">
-                <img src="src/img/icons/instagram.svg" alt="Instagram Al Ashr Al Madani" class="w-8 h-8">
-                <img src="src/img/icons/facebook.svg" alt="Facebook Al Ashr Al Madani" class="w-8 h-8">
-                <img src="src/img/icons/youtube.svg" alt="Youtube Al Ashr Al Madani" class="w-8 h-8">
-                <img src="src/img/icons/twitter.svg" alt="Youtube Al Ashr Al Madani" class="w-8 h-8">
-                <img src="src/img/icons/whatsapp.svg" alt="Youtube Al Ashr Al Madani" class="w-8 h-8">
+                <img src="src/img/icons/instagram.svg" alt="Instagram Al Ashr Al Madani" class="w-8 h-8 md:w-5 md:h-5">
+                <img src="src/img/icons/facebook.svg" alt="Facebook Al Ashr Al Madani" class="w-8 h-8 md:w-5 md:h-5">
+                <img src="src/img/icons/youtube.svg" alt="Youtube Al Ashr Al Madani" class="w-8 h-8 md:w-5 md:h-5">
+                <img src="src/img/icons/twitter.svg" alt="Youtube Al Ashr Al Madani" class="w-8 h-8 md:w-5 md:h-5">
+                <img src="src/img/icons/whatsapp.svg" alt="Youtube Al Ashr Al Madani" class="w-8 h-8 md:w-5 md:h-5">
             </div>
         </div>
-        <div class="jenjang-pendidikan mt-5 w-full">
-            <h2 class="text-center text-2xl font-semibold">Jenjang Pendidikan</h2>
-            <div class="items-pendidikan text-center mt-2 flex flex-row justify-center items-center gap-5">
+        <div class="jenjang-pendidikan mt-5 md:m-0 w-full md:w-[50%] lg:w-[30%]">
+            <h2 class="text-center md:text-left text-2xl sm:text-xl font-semibold md:mt-4">Jenjang Pendidikan</h2>
+            <div class="items-pendidikan md:text-left text-center mt-2 flex flex-row justify-center items-center gap-5 lg:justify-start md:items-start md:flex-col md:gap-2">
                 <a href="#">SMP</a>
                 <a href="#">SMATER</a>
             </div>
         </div>
         <!-- informasi -->
-        <div class="informasi w-full text-center mt-5 ">
-            <h2 class="text-center text-2xl font-semibold">Informasi</h2>
-            <div class="text-center mt-2 flex flex-col justify-center items-center gap-2">
+        <div class="informasi w-full lg:w-[30%] md:w-[30%] text-center mt-5 md:mt-0">
+            <h2 class="text-center md:text-left text-2xl font-semibold md:mt-4 sm:text-xl">Informasi</h2>
+            <div class="text-center md:text-left mt-2 flex flex-col justify-center items-center gap-2 md:items-start">
                 <a href="#">Seputar Pertanyaan</a>
                 <a href="#">Pendaftaran</a>
                 <a href="#">Karir</a>
@@ -89,7 +89,7 @@
         </div>
     </div>
 
-    <div class="copyright text-dark-font bg-body h-10 flex flex-row justify-center items-center">
-        <h2 class="text-center text-sm "><span class="font-bold">&copy;</span> 2023 Al ‘Ashr Al Madani. All right reserved.</h2>
+    <div class="copyright text-dark-font bg-white h-10 md:h-12 lg:h-14 flex flex-row justify-center items-center">
+        <h2 class="text-center text-sm font-semibold md:text-lg lg:text-xl "><span class="font-bold">&copy;</span> 2023 Al ‘Ashr Al Madani. All right reserved.</h2>
     </div>
 </footer>
