04f29ff3bfd043b79daee2ef16dbe64ae7371160
Narapati24 <narapati.223040155@mail.unpas.ac.id>
2023-08-10 11:14:58 +0700

    NEWS [dynamic news]

--- src/backend/partials/card-berita.php
+++ src/backend/partials/card-berita.php
@@ -1,3 +1,7 @@
+<?php
+$news = query("SELECT * FROM news ORDER BY nws_id DESC");
+?>
+
 <section class="text-main-purple  w-full  py-12" id="berita">
     <h2 class="font-bold text-4xl px-[5%] text-center">Berita Terbaru</h2>
     <p class="text-base px-[5%] tracking-widest text-center lg:text-2xl mt-1 sm:text-xl">
@@ -5,57 +9,38 @@
     </p>
     <div class="mt-10 sm:flex sm:flex-row sm:flex-wrap sm:justify-center sm:gap-10">
         <!-- desktop -->
-        <div class="rounded-lg overflow-hidden sm:shadow-lg hidden sm:w-80 sm:block lg:w-[350px] h-80 bg-white">
-            <img src="https://placeholder.com/440x230" alt="news" />
-            <div class="p-5">
-                <h3 class="font-bold text-2xl">Kajian Rutinan</h3>
-                <p class="text-base">
-                    Pengajian rutinan Ibu-ibu membaca Alquran, mendalami Hadis, ilmu
-                    Tauhid, Akhlak dan Fiqh,
-                </p>
+        <?php foreach ($news as $new) : ?>
+            <div class="rounded-lg overflow-hidden sm:shadow-lg hidden sm:w-80 sm:block lg:w-[350px] h-80 bg-white">
+                <img src="./src/img/uploaded/berita/<?= $new['nws_img']; ?>" alt="<?= $new['nws_title']; ?>" />
+                <div class="p-5">
+                    <h3 class="font-bold text-2xl"><?= $new['nws_title']; ?></h3>
+                    <p class="text-base">
+                        <?= $new['nws_short_desc']; ?>
+                    </p>
+                </div>
             </div>
-        </div>
-        <div class="rounded-lg overflow-hidden sm:shadow-lg mb-10 hidden sm:w-80 sm:block lg:w-[350px] h-80 bg-white">
-            <img src="https://placeholder.com/440x230" alt="news" />
-            <div class="p-5">
-                <h3 class="font-bold text-2xl">Pemeriksaan Barang</h3>
-                <p class="text-base">
-                    Pemeriksaan barang bawaan santri sebelum masuk pondok pesantren
-                </p>
-            </div>
-        </div>
+        <?php endforeach; ?>
 
         <!-- mobile -->
         <div class="swiper swiper-berita sm:hidden">
             <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <div class=" w-[85%] mx-auto rounded-xl overflow-hidden border-2  bg-white">
-                        <img src="https://placeholder.com/440x230" alt="news"  class="w-full"/>
-                        <div class="px-5 py-10">
-                            <h3 class="font-bold text-2xl">Kajian Rutinan</h3>
-                            <p class="text-base">
-                                Pengajian rutinan Ibu-ibu membaca Alquran, mendalami
-                                Hadis, ilmu Tauhid, Akhlak dan Fiqh,
-                            </p>
+                <?php foreach ($news as $new) : ?>
+                    <div class="swiper-slide">
+                        <div class=" w-[85%] mx-auto rounded-xl overflow-hidden border-2  bg-white">
+                            <img src=".src/img/uploaded/berita/<?= $new['nws_img']; ?>" alt="<?= $new['nws_title']; ?>" class="w-full" />
+                            <div class="px-5 py-10">
+                                <h3 class="font-bold text-2xl"><?= $new['nws_title']; ?></h3>
+                                <p class="text-base">
+                                    <?= $new['nws_short_desc']; ?>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class=" w-[85%] mx-auto rounded-xl overflow-hidden border-2  bg-white">
-                        <img src="https://placeholder.com/440x230" alt="news"  class="w-full"/>
-                        <div class="px-5 py-10">
-                            <h3 class="font-bold text-2xl">Kajian Rutinan</h3>
-                            <p class="text-base">
-                                Pengajian rutinan Ibu-ibu membaca Alquran, mendalami
-                                Hadis, ilmu Tauhid, Akhlak dan Fiqh,
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                <?php endforeach; ?>
             </div>
             <!-- If we need pagination -->
             <div class="swiper-pagination top-10"></div>
-            
+
             <!-- If we need navigation buttons -->
             <div class="swiper-button-prev">
                 <img src="./src/img/arrow-left.svg" alt="left" />
