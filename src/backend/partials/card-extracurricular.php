915e842d6778eb9c62e4bf2e157fc194cdd38a28
Hilmianugrah <hilmianugrah2003@gmail.com>
2023-08-12 16:03:15 +0700

    merapihkan navbar

--- src/backend/partials/card-extracurricular.php
+++ src/backend/partials/card-extracurricular.php
@@ -89,10 +89,21 @@
   <!-- end desktop -->
 
   <!-- mobile -->
-  <swiper-container class="mySwiper display-extracurricular w-[85%] h-[50%] sm:w-[70%] sm:h-[300px]" effect="cards" grab-cursor="true">
+  <swiper-container class="mySwiper display-extracurricular  w-[85%] h-[200px] sm:w-[70%] sm:h-[280px]" effect="cards" grab-cursor="true">
+    <swiper-slide style="background-color: #77B341;" class=" overflow-hidden ">
+      <div>
+        <img class="relative z-10 bg-cover" src="./src/img/uploaded/ekstrakurikuler/pencaksilat.png" alt="pencaksilat" loading="lazy">
+        <div class="title-extracurricular flex flex-col justify-center  z-10 absolute bottom-0 left-0 h-[40px] w-full text-center items-center sm:h-[60px]">
+          <h3 class="m-auto ">Pencaksilat</h3>
+        </div>
+        <img src="src/img/program_pilihan/Vector.png" alt="al ashr al madani" class="bg-cover absolute top-[-15px] right-[-30px] w-[40%] " />
+        <img src="src/img/program_pilihan/Vector.png" alt="" class="bg-cover absolute bottom-[10px] left-[-10px] w-[40%]" />
+
+      </div>
+    </swiper-slide>
     <swiper-slide style="background-color: #77B341;" class=" overflow-hidden">
       <div>
-        <img class="relative z-10" src="./src/img/uploaded/ekstrakurikuler/pencaksilat.png" alt="pencaksilat" loading="lazy">
+        <img class="relative z-10  bg-cover" src="./src/img/hilmi.png" alt="pencaksilat" loading="lazy">
         <div class="title-extracurricular flex flex-col justify-center  z-10 absolute bottom-0 left-0 h-[40px] w-full text-center items-center sm:h-[60px]">
           <h3 class="m-auto ">Pencaksilat</h3>
         </div>
@@ -103,18 +114,7 @@
     </swiper-slide>
     <swiper-slide style="background-color: #77B341;" class=" overflow-hidden">
       <div>
-        <img class="relative z-10" src="./src/img/uploaded/ekstrakurikuler/pencaksilat.png" alt="pencaksilat" loading="lazy">
-        <div class="title-extracurricular flex flex-col justify-center  z-10 absolute bottom-0 left-0 h-[40px] w-full text-center items-center sm:h-[60px]">
-          <h3 class="m-auto ">Pencaksilat</h3>
-        </div>
-        <img src="src/img/program_pilihan/Vector.png" alt="al ashr al madani" class="bg-cover absolute top-[-25px] right-[-30px] w-[40%] " />
-        <img src="src/img/program_pilihan/Vector.png" alt="" class="bg-cover absolute bottom-[10px] left-[-10px] w-[40%]" />
-
-      </div>
-    </swiper-slide>
-    <swiper-slide style="background-color: #77B341;" class=" overflow-hidden">
-      <div>
-        <img class="relative z-10" src="./src/img/uploaded/ekstrakurikuler/pencaksilat.png" alt="pencaksilat" loading="lazy">
+        <img class="relative z-10" src="./src/img/panahan.png" alt="pencaksilat" loading="lazy">
         <div class="title-extracurricular flex flex-col justify-center  z-10 absolute bottom-0 left-0 h-[40px] w-full text-center items-center sm:h-[60px]">
           <h3 class="m-auto ">Pencaksilat</h3>
         </div>
