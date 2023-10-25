2e2209025e5bd42449715205ccc81299719518f1
aldiipratama <paldi0013@gmail.com>
2023-10-25 09:49:42 +0700

    view data

--- src/backend/partials/ajax/load/view-data.php
+++ src/backend/partials/ajax/load/view-data.php
@@ -1,26 +1,26 @@
-<section id="view-data" class=" w-full h-[100vh] fixed top-0 bg-[rgb(0,0,0,0.4)] #hidden-data z-[9999999]">
+<section id="view-data" class=" w-full h-[100vh] fixed top-0 bg-[rgb(0,0,0,0.4)] hidden-data z-[9999999]">
     <button id="closeview" class="fixed top-0 right-0 z-50 block border-none rounded-lg close w-14 h-14 md:mr-20 lg:mr-60"><img src="<?= baseUrl("src/img/icons/close.svg"); ?>" class="justify-end block w-full text-main" alt="close"></button>
     <div class="w-[95%] md:w-[80%] lg:w-[60%] absolute bg-body px-5 py-10 bottom-0 left-1/2 -translate-x-1/2 overflow-y-scroll rounded-t-lg content-data-view">
         <h1 class="max-w-lg mx-auto text-2xl font-semibold text-center">FORMULIR ISIAN PONDOK PESANTREN TAHFIDZUH QURAN AL-’ASHR AL-MADANI</h1>
         <div class="p-5 border border-slate-900">
-            <table class="text-left table-auto font-['Poppins']">
-                <tr class="border-none">
-                    <th>Data</th>
+            <table class="text-left table-auto font-['Poppins'] w-full">
+                <tr>
+                    <td colspan="3">Data</td>
                 </tr>
                 <tr>
-                    <td class="font-medium text-left border-none">No. Peserta Test</td>
-                    <td class="px-2 font-medium text-left border-none">:</td>
-                    <td class="font-medium text-left border-none">1</td>
+                    <td>No. Peserta Test</td>
+
+                    <td>: 1</td>
                 </tr>
                 <tr>
-                    <td class="font-medium text-left border-none">Nama Lengkap Anak (Calon Siswa/Santri)</td>
-                    <td class="px-2 font-medium text-left border-none">:</td>
-                    <td class="font-medium text-left border-none">Hilmi Anugrah Bela negara</td>
+                    <td>Nama Lengkap Anak (Calon Siswa/Santri)</td>
+
+                    <td>: Hilmi Anugrah Bela negara</td>
                 </tr>
                 <tr>
-                    <td class="font-medium text-left border-none">Tanggal lahir</td>
-                    <td class="px-2 font-medium text-left border-none">:</td>
-                    <td class="font-medium text-left border-none">Bandung, 11 Mei 2003</td>
+                    <td>Tanggal lahir</td>
+
+                    <td>: Bandung, 11 Mei 2003</td>
                 </tr>
             </table>
         </div>
@@ -29,70 +29,274 @@
             <li>
                 <h2>A. Data Anak</h2>
                 <div class="p-5 border border-slate-900">
-                    <table class="text-left table-auto font-['Poppins']">
+                    <table class="table-auto">
                         <tr>
-                            <td class="w-full border-none" rowspan="6"><img src="https://placeholder.com/400x600" alt="pas foto"></td>
+                            <td colspan="8" class="lg:hidden mx-auto">
+                                <img src="https://placeholder.com/400x600" alt="pas foto">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td rowspan="6" class="max-lg:hidden">
+                                <img src="https://placeholder.com/400x600" alt="pas foto">
+                            </td>
+                            <td>
+                                Nama Lengkap
+                            </td>
+
+                            <td colspan="3">: Hilmi Anugrah Bela Negara</td>
 
                         </tr>
                         <tr>
-                            <td class="font-medium text-left border-none">
-                                Nama Lengkap
-                            </td>
-                            <td class="font-medium border-none">:</td>
-                            <td class="font-medium text-left border-none" colspan="3">Hilmi Anugrah Bela Negara</td>
-                            <td class="font-medium text-left border-none">jenis Kelamin</td>
-                            <td class="font-medium border-none">:</td>
-                            <td class="font-medium text-left border-none">
-                                Laki-Laki
+                            <td>jenis Kelamin</td>
+
+                            <td>
+                                : Laki-Laki
                             </td>
                         </tr>
                         <tr>
-                            <td class="font-medium text-left border-none">
+                            <td>
                                 Nama Panggilan
                             </td>
-                            <td class="font-medium border-none">:</td>
-                            <td class="font-medium text-left border-none">
-                                Hilmi
+
+                            <td colspan="6">
+                                : Hilmi
                             </td>
                         </tr>
                         <tr>
-                            <td class="font-medium text-left border-none">
+                            <td>
                                 Tempat Tanggal Lahir
                             </td>
-                            <td class="font-medium border-none">:</td>
-                            <td class="font-medium text-left border-none" colspan="3">Bandung, 11 Mei 2003</td>
+
+                            <td colspan="6">: Bandung, 11 Mei 2003</td>
                         </tr>
                         <tr>
-                            <td class="font-medium text-left border-none">Alamat Rumah</td>
-                            <td class="font-medium border-none">:</td>
-                            <td class="font-medium text-left border-none" colspan="3">Jl. Arcamanik, Sindanglaya, Kec. Cimenyan, Kota Bandung, Jawa Barat 40195</td>
+                            <td>Alamat Rumah</td>
+
+                            <td colspan="6">: Jl. Arcamanik, Sindanglaya, Kec. Cimenyan, Kota Bandung, Jawa Barat 40195</td>
                         </tr>
                         <tr>
-                            <td class="font-medium text-left border-none">Kode Pos</td>
-                            <td class="font-medium border-none ">:</td>
-                            <td class="font-medium text-left border-none">40195</td>
+                            <td>Kode Pos</td>
+
+                            <td colspan="6">: 40195</td>
                         </tr>
                         <tr>
-                            <td class="font-medium text-left border-none">Telp Rumah</td>
-                            <td class="font-medium border-none">:</td>
-                            <td class="font-medium text-left border-none">223040175</td>
-                            <td class="font-medium text-left border-none">Kecamatan</td>
-                            <td class="font-medium border-none">:</td>
-                            <td class="font-medium text-left border-none">Ujung Berung</td>
+                            <td>Telp Rumah</td>
+
+                            <td colspan="2">: 223040175</td>
+
                         </tr>
                         <tr>
-                            <td class="font-medium text-left border-none">Bahasa Sehari-hari</td>
-                            <td class="font-medium border-none">:</td>
-                            <td class="font-medium text-left border-none">Bahasa Indonesia</td>
-                            <td class="font-medium text-left border-none">Kelurahan</td>
-                            <td class="font-medium border-none">:</td>
-                            <td class="font-medium text-left border-none">Pasanggrahan</td>
+                            <td>Kecamatan</td>
+
+                            <td colspan="2">: Ujung Berung</td>
                         </tr>
                         <tr>
-                            <td class="font-medium text-left border-none">Jarak Tempuh dari Rumah ke Sekolah</td>
-                            <td class="font-medium border-none">:</td>
-                            <td class="font-medium text-left border-none">... KM</td>
-                            <td class="font-medium text-left border-none" colspan="5">Anak ke ___ dari ___ Bersaudara</td>
+                            <td>Bahasa Sehari-hari</td>
+
+                            <td colspan="2">: Bahasa Indonesia</td>
+
+                        </tr>
+                        <tr>
+                            <td>Kelurahan</td>
+
+                            <td colspan="2">: Pasanggrahan</td>
+                        </tr>
+                        <tr>
+                            <td>Jarak Tempuh dari Rumah ke Sekolah</td>
+
+                            <td colspan="2">: ... KM</td>
+
+                        </tr>
+                        <tr>
+                            <td colspan="3">Anak ke ___ dari ___ Bersaudara</td>
+                        </tr>
+                    </table>
+                </div>
+            </li>
+            <li class="mt-5">
+                <h2>B. Orang Tua</h2>
+                <div class="p-5 border border-slate-900">
+                    <table class="table-auto w-full">
+                        <tr>
+                            <td></td>
+                            <td class="!text-center">Ayah</td>
+                            <td class="!text-center">Ibu</td>
+                        </tr>
+                        <tr>
+                            <td>Nama</td>
+                            <td>: Aldi Pratama</td>
+                            <td>: Lorem, ipsum dolor.</td>
+                        </tr>
+                        <tr>
+                            <td>Tempat tanggal lahir</td>
+                            <td>: Bandung, 01 Agustus 2002</td>
+                            <td>: Bandung, 08 Oktober 2003</td>
+                        </tr>
+                        <tr>
+                            <td>Menikah pada usia</td>
+                            <td>: -</td>
+                            <td>: -</td>
+                        </tr>
+                        <tr>
+                            <td>Pernikahan ke</td>
+                            <td>: -</td>
+                            <td>: -</td>
+                        </tr>
+                        <tr>
+                            <td>Status Pernikahan</td>
+                            <td colspan="2">: Nikah / Cerai</td>
+                        </tr>
+                        <tr>
+                            <td>Telp Rumah</td>
+                            <td>:</td>
+                            <td>:</td>
+                        </tr>
+                        <tr>
+                            <td>Hp</td>
+                            <td>: 085798257393</td>
+                            <td>: 085798257393</td>
+                        </tr>
+                        <tr>
+                            <td>Whatsapp / Pin BBM</td>
+                            <td>: 085798257393 / -</td>
+                            <td>: 085798257393 / -</td>
+                        </tr>
+                        <tr>
+                            <td>Alamat Email</td>
+                            <td>: paldi0013@gmail.com</td>
+                            <td>: contoh@gmailcom</td>
+                        </tr>
+                        <tr>
+                            <td colspan="3">Riwayat pendidikan :</td>
+                        </tr>
+                        <tr>
+                            <td>SMP</td>
+                            <td>:</td>
+                            <td>:</td>
+                        </tr>
+                        <tr>
+                            <td>SMA</td>
+                            <td>:</td>
+                            <td>:</td>
+                        </tr>
+                        <tr>
+                            <td>Perguruan Tinggi</td>
+                            <td>: Universitas Pasundan</td>
+                            <td>: Universitas Pasundan</td>
+                        </tr>
+                        <tr>
+                            <td>Jurusan / Fakultas</td>
+                            <td>: Informatika / Teknik</td>
+                            <td>: Informatika / Teknik</td>
+                        </tr>
+                        <tr>
+                            <td>Pekerjaan / Profesi</td>
+                            <td>:</td>
+                            <td>:</td>
+                        </tr>
+                        <tr>
+                            <td>Jabatan</td>
+                            <td>:</td>
+                            <td>:</td>
+                        </tr>
+                        <tr>
+                            <td>Alamat Kantor</td>
+                            <td>:</td>
+                            <td>:</td>
+                        </tr>
+                        <tr>
+                            <td>Telepon Kantor</td>
+                            <td>:</td>
+                            <td>:</td>
+                        </tr>
+                        <tr>
+                            <td>Alamat Rumah jika sehari-hari tinggal berbeda dengan anak</td>
+                            <td>:</td>
+                            <td>:</td>
+                        </tr>
+                    </table>
+                </div>
+            </li>
+            <li class="mt-5">
+                <h2>C. KELUARGA LAINNYA</h2>
+                <div class="p-5 border border-slate-900">
+                    <table class="table-auto w-full">
+                        <tr>
+                            <td colspan="3">
+                                Apakah ada orang lain / keluarga yang tinggal di rumah ? Siapa ? Apa hubungannya dengan siswa / santri ?
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Siapakah orang terdekat yang dapat dihubungi, jika orang tua tidak ada ?</td>
+                            <td></td>
+                            <td>Siapakah yang secara konsisten bertanggung jawab untuk berkomunikasi dengan pihak pesantren / sekolah jika orang tua berhalangan ? (wali)</td>
+                        </tr>
+                        <tr>
+                            <td>Nama</td>
+                            <td>:</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Hubungan</td>
+                            <td>:</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Alamat</td>
+                            <td>:</td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Telp. Rumah / kantor / No. HP</td>
+                            <td>:</td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+            </li>
+            <li class="mt-5">
+                <h2>D. RIWAYAT KESEHATAN ANAK</h2>
+                <div class="p-5 border border-slate-900">
+                    <table class="table-fixed w-full">
+                        <tr>
+                            <td>Tinggi badan saat ini</td>
+                            <td>: 150</td>
+                        </tr>
+                        <tr>
+                            <td>Berat badan saat ini</td>
+                            <td>: 48</td>
+                        </tr>
+                        <tr>
+                            <td>Keluhan fisik</td>
+                            <td>Mata :</td>
+                            <td>Telinga :</td>
+                        </tr>
+                        <tr>
+                            <td>Keluhan lain yang sering disampaikan anak</td>
+                            <td>:</td>
+                        </tr>
+                        <tr>
+                            <td>Riwayat terapi</td>
+                            <td>:</td>
+                        </tr>
+                        <tr>
+                            <td>Pernah dirawat di rumah sakit ? jika ya, jelaskan</td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+            </li>
+            <li class="mt-5">
+                <p>Hal penting yang menurut orang tua perlu diketahui pihak Pesantren / SMP Plus mengenai perkembangan anak :</p>
+                <div class="p-5 border border-slate-900">
+                    <table class="table-auto w-full">
+                        <tr>
+                            <td>Golongan darah : O</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum consectetur iusto, voluptas sunt commodi cum. Sint cupiditate tempora natus quae laboriosam dolore sit eligendi officia, aperiam sequi aspernatur molestiae adipisci cumque, iste distinctio quis obcaecati veritatis consectetur. Odit vero perspiciatis porro. Error corrupti reiciendis eaque. Doloremque exercitationem maiores beatae nobis?
+                            </td>
                         </tr>
                     </table>
                 </div>
